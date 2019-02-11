edd2c34d2be7fffdd7c385e31cb5b44301a33311
Miguel <miguel.collado@kairosds.com>
2019-02-11 19:27:50 +0100

    fix(server): fix and add more tests

--- src/server/__tests__/index.js
+++ src/server/__tests__/index.js
@@ -4,12 +4,32 @@ const api = request(app);
 const BASE_URL = '/phones';
 
 describe('Server', () => {
-  test(`[GET ${BASE_URL}] Should list all phones with status 200`, async () => {
+  test(`[GET ${BASE_URL}] with a page size equal to the data size should list all phones with status 200`, async () => {
     const response = await api
-      .get(BASE_URL)
+      .get(`${BASE_URL}?page=1&itemsPerPage=20`)
+      .expect(200);
+    console.log(response.body.phones)
+    expect(response.body.phones).toHaveLength(20);
+  });
+  
+  test(`[GET ${BASE_URL}] with a page size of 10 gives us the first 10 elements`, async () => {
+    const response = await api
+      .get(`${BASE_URL}?page=1&itemsPerPage=10`)
       .expect(200);
 
-    expect(response.body.phones).toHaveLength(3);
+    expect(response.body.phones).toHaveLength(10);
+    expect(response.body.paginator.pages).toBe(2);
+    expect(response.body.paginator.pageNumber).toBe("1");
+  });
+
+  test(`[GET ${BASE_URL}] with a page size of 10 and a page of 2 gives us the last 10 elements`, async () => {
+    const response = await api
+      .get(`${BASE_URL}?page=2&itemsPerPage=10`)
+      .expect(200);
+    console.log(response.body.phones)
+    expect(response.body.phones).toHaveLength(10);
+    expect(response.body.paginator.pages).toBe(2);
+    expect(response.body.paginator.pageNumber).toBe("2");
   });
   
   test(`[GET ${BASE_URL}/0] Should get the phone with id '0'`, async () => {
