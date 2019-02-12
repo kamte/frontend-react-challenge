440ab2b04fb9b31703dee8f69500df20622bda32
Miguel <miguel.collado@kairosds.com>
2019-02-12 08:21:19 +0100

    add(test): add back button test

--- client/src/components/PhoneDetail/index.test.js
+++ client/src/components/PhoneDetail/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import PhoneDetail from '.';
 import sinon from 'sinon';
 
@@ -19,4 +19,11 @@ test('Should call its onFetchPhone function when mounted', () => {
     const onFetchPhone = sinon.spy();
     shallow(<PhoneDetail onFetchPhone={onFetchPhone} match={match} title="test" price="232.5" description="Lorem" img="http://lorempixel.com/640/480/technics?77029"/>);
     expect(onFetchPhone.callCount).toEqual(1);
+});
+
+test('The back button should have a link to the main page', () => {
+    const match = {params: {id: 0}}
+    const onFetchPhone = () => {};
+    const component = shallow(<PhoneDetail onFetchPhone={onFetchPhone} match={match} title="test" price="232.5" description="Lorem" img="http://lorempixel.com/640/480/technics?77029"/>);
+    expect(component.find('Link').prop('to')).toEqual('/')
 });
